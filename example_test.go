c95d7d624d90d0dc0aa8c605b0602d2501586353
M. J. Fromberger <michael.j.fromberger@gmail.com>
2025-11-02 20:43:20 -0800

    keyring: add a package example

---
+++ example_test.go
@@ -0,0 +1,61 @@
+// Copyright (C) 2025 Michael J. Fromberger. All Rights Reserved.
+
+package keyring_test
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+
+	"github.com/creachadair/keyring"
+)
+
+func Example() {
+	key, salt := keyring.AccessKeyFromPassphrase("hunter2")
+
+	r, err := keyring.New(keyring.Config{
+		AccessKey:     key,
+		AccessKeySalt: salt,
+		InitialKey:    []byte("too many secrets"),
+	})
+	if err != nil {
+		log.Fatalf("New failed: %v", err)
+	}
+
+	// Print the currently-active key.
+	fmt.Printf("Key %d: %q\n", r.Active(), r.Get(0))
+
+	// Add another key and print that too.
+	id := r.Add([]byte("no more secrets"))
+	fmt.Printf("Key %d: %q\n", id, r.Get(id))
+
+	// Note that the active key ID doesn't change until we say so.
+	fmt.Printf("Active ID before: %d\n", r.Active())
+	r.Activate(id)
+	fmt.Printf("Active ID after: %d\n", r.Active())
+
+	var buf bytes.Buffer
+	nw, err := r.WriteTo(&buf)
+	if err != nil {
+		log.Fatalf("Write failed: %v", err)
+	}
+	fmt.Printf("Encoded keyring is %d bytes\n\n", nw)
+
+	// Read the keyring back in from "storage" (buf).
+	r2, err := keyring.Read(&buf, keyring.PassphraseKey("hunter2"))
+	if err != nil {
+		log.Fatalf("Read failed: %v", err)
+	}
+	fmt.Println("(reloaded)")
+	fmt.Printf("Key %d: %q\n", r2.Active(), r2.Get(0))
+
+	// Output:
+	// Key 1: "too many secrets"
+	// Key 2: "no more secrets"
+	// Active ID before: 1
+	// Active ID after: 2
+	// Encoded keyring is 215 bytes
+	//
+	// (reloaded)
+	// Key 2: "no more secrets"
+}
